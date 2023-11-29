121234f619accb8de48bac292d67d5607dba7490
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2023-11-29 19:18:30 +0100

    @EnableEurekaClient

--- src/main/java/tn/esprit/microchats/MicroChatsApplication.java
+++ src/main/java/tn/esprit/microchats/MicroChatsApplication.java
@@ -2,8 +2,10 @@ package tn.esprit.microchats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroChatsApplication {
 
     public static void main(String[] args) {
